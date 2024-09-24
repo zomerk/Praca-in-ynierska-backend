532b1afcac047ede9513074d197b78c4397f2434
zomerk <zomerfeldkacper@gmail.com>
2024-09-24 20:32:56 +0200

    setting up

---
+++ src/main/java/com/example/pracainzynierska/PracaInzynierskaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pracainzynierska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaInzynierskaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaInzynierskaApplication.class, args);
+    }
+
+}
